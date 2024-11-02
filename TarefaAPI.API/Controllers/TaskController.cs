25f7126915f0dd0aea0985502575b3643058d25a
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-02 01:41:30 -0300

    Adicionar arquivos de projeto.

---
+++ TarefaAPI.API/Controllers/TaskController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefaChallenge.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+}
